5eb8f68888b904e17b7acadc0d72ae80f1b6a72f
rachitchaurasia17-ctrl <rachitchaurasia17@gmail.com>
2026-06-11 16:00:11 +0530

    Fix Realty suffix and update correct address

--- components/PropertyAI.jsx
+++ components/PropertyAI.jsx
@@ -11,7 +11,7 @@ const SYSTEM_PROMPT = `You are Property AI, the digital concierge for Chandigarh
 
 Firm facts:
 - Firm: Chandigarh Real Estate
-- Office: SCO 1, 2nd Floor, Sector 79, Sahibzada Ajit Singh Nagar, Punjab 140308
+- Office: Sector 36-D, Chandigarh
 - Phone: +91 XXXXX XXXXX
 - GMADA empanelled, RERA registered (PB-RERA)
 - Practice: Residential, Commercial, Land/Plots, Investment Advisory, NRI Services, Title & Due Diligence
